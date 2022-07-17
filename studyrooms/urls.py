8422681970e159a6da3826992961f3b32802f278
Ali-Sayidi <thealisayidi@gmail.com>
2022-07-17 14:40:40 +0430

    project created

---
+++ studyrooms/urls.py
@@ -0,0 +1,10 @@
+# studyrooms URL Configuration
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', include('base.urls')),
+    path('admin/', admin.site.urls),
+]
